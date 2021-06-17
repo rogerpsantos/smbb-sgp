fee4dcdd27567c3791205f6d38f0580915f9d772
smbb-consultoria <56878404+smbb-consultoria@users.noreply.github.com>
2021-06-17 11:18:21 -0300

    Atualização

--- src/pages/main/Home.js
+++ src/pages/main/Home.js
@@ -8,6 +8,7 @@ export default class Home extends Component {
         <a href="/federal"> <img src="brazil.png" alt="Obrigações Federais"></img> </a>
         <a href="/estadual"> <img src="estados.png" alt="Obrigações Estaduais"></img> </a>
         <a href="/municipal"> <img src="prefeitura.png" alt="Obrigações Municipais"></img> </a>
+        {/* TESTE */}
       </div>
       );
   }
